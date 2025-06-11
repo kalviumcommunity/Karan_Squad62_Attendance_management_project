3391bc5e4460958316bda346f45e0b1491ada840
Karan-1208 <mkarandeep.1208@gmail.com>
2025-06-11 12:57:23 +0530

    WIP: saving local changes before rebase

--- Desktop/Karan_Squad62_Attendance_management_project-main/src/com/school/Main.java
+++ Desktop/Karan_Squad62_Attendance_management_project-main/src/com/school/Main.java
@@ -2,7 +2,7 @@ package com.school;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Welcome to the Attendace Management System!");
+        System.out.println("Welcome to the Attendance Management System!");
     }
 }
 
